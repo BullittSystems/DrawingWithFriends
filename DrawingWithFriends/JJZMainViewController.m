ec854eb5d94c67cc312a322c567d563375510e78
Ryan C. Payne <rpayne@bullittsystems.com>
2016-06-09 08:04:54 -0700

    Rough beginnings of the convo buttons.

--- DrawingWithFriends/JJZMainViewController.m
+++ DrawingWithFriends/JJZMainViewController.m
@@ -19,6 +19,8 @@
 
 @interface JJZMainViewController () <JJZCanvasViewDelegate, JJZPeopleManagerDelegate, TwilioAccessManagerDelegate, JJZDrawingManagerDelegate>
 @property (strong, nonatomic) IBOutlet JJZCanvasView *canvasView;
+@property (weak, nonatomic) IBOutlet UIButton *endConversationButton;
+@property (weak, nonatomic) IBOutlet UIButton *inviteButton;
 
 @property (strong, nonatomic) TwilioAccessManager *accessManger;
 
@@ -40,6 +42,10 @@
 
     self.canvasView.delegate = self;
     [self changeColor:self];
+
+    // Initial UI State
+    self.endConversationButton.enabled = NO;
+    self.inviteButton.enabled = NO;
 
     typeof(self) __weak weakSelf = self;
     [JJZCredentialManager retrieveAccessTokenWithCompletionBlock:^(NSString *accessToken, NSError *error) {
@@ -66,6 +72,52 @@
     [self.drawingManager clearDrawing];
 }
 
+- (IBAction)invite:(id)sender {
+    if ([self.inviteButton.titleLabel.text isEqualToString:@"Invite"]) {
+        [self.inviteButton setTitle:@"Invite Another" forState:UIControlStateNormal];
+    } else {
+        [self.inviteButton setTitle:@"Invite" forState:UIControlStateNormal];
+    }
+
+    [self.inviteButton sizeToFit];
+}
+
+- (IBAction)end:(id)sender {
+    if ([self.endConversationButton.titleLabel.text isEqualToString:@"End Conversation"]) {
+        [self.endConversationButton setTitle:@"Leave Converation" forState:UIControlStateNormal];
+    } else {
+        [self.endConversationButton setTitle:@"End Converation" forState:UIControlStateNormal];
+    }
+
+    [self.endConversationButton sizeToFit];
+}
+
+- (void)updateUI {
+    // This method will toggle the buttons according to state.
+    // If we are part of a Converation and we started it, "End Converation"
+    // If we are part of a Converation as a participant, "Leave Converation"
+
+    // If no current converstation:
+    // If nobody is online : Invite - Enabled = NO
+    // If online and available : Invite - Enabled = YES
+
+    // If part of a conversation:
+    // If not the creator : Invite - Enabled = NO
+    // If the creator and only one person
+
+    NSUInteger availableCount = 0;
+
+    for (JJZPerson *person in self.peopleManager.availablePeople) {
+        if (person.isOnline && person.isAvailable) {
+            availableCount++;
+        }
+    }
+
+    self.inviteButton.enabled = (availableCount > 0);
+
+
+}
+
 #pragma mark - JJZCanvasViewDelegate
 - (void)canvasView:(JJZCanvasView *)canvasView didFinishDrawingPath:(JJZDrawingPath *)drawingPath {
     DFlog(@"Send this path! %@", drawingPath);
@@ -84,14 +136,17 @@
 #pragma mark - JJZPeopleManagerDelegate
 - (void)peopleManager:(JJZPeopleManager *)peopleManager didAddPerson:(JJZPerson *)person {
     DFlog(@"Added person: %@", [person debugDescription]);
+    [self updateUI];
 }
 
 - (void)peopleManager:(JJZPeopleManager *)peopleManager didUpdatePerson:(JJZPerson *)person {
     DFlog(@"Updated person: %@", [person debugDescription]);
+    [self updateUI];
 }
 
 - (void)peopleManager:(JJZPeopleManager *)peopleManager didRemovePerson:(JJZPerson *)person {
     DFlog(@"Removed person: %@", [person debugDescription]);
+    [self updateUI];
 }
 
 #pragma mark - TwilioAccessManagerDelegate
